8126d0fab6c089648514e3b55970f0b021cbac10
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-03-25 16:54:29 +0530

    done

---
+++ gsap/script.js
@@ -0,0 +1,46 @@
+// gsap.to("#box",{
+//     x:1000,
+//     duration:5,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+//     scale:2
+// })
+
+// gsap.to("#box2",{
+//     x:1200,
+//     duartion:3,
+//     delay:6,
+//     rotate:360,
+    
+// })
+
+// gsap.to("#box3",{
+//     x:1200,
+//     duartion:3,
+//     delay:9,
+//     rotate:360,
+   
+
+// })
+
+var k=gsap.timeline()
+k.to("#box",{
+    x:1500,
+    duration:4,
+    delay:2,
+
+})
+k.to("#box2",{
+    x:1500,
+    duration:4,
+    delay:2,
+
+})
+k.to("#box3",{
+    x:1500,
+    duration:4,
+    delay:2,
+
+})
