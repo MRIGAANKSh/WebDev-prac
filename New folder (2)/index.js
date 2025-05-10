7b305260c78ccfd99779be7a34d1829b9b6efaf4
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-05-10 15:53:39 +0530

    form handling

---
+++ New folder (2)/index.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const fs=require('fs')
+const app=express()
+const path=require('path')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs')
+
+
+
+app.get('/',function(req,res){
+    fs.readdir(`./files`,function(err,files){
+        console.log(files)
+        res.render("index",{files:files});
+    })
+    
+    console.log("server runnign on port 3000")
+})
+
+
+
+app.post('/create',function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        if(err){
+            console.log("error creating file")
+        }
+        else{
+            console.log("file created")
+            res.redirect("/")
+            
+        }
+    })
+})
+
+app.listen(3000)
