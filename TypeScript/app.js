fd8caa22fbccbe5e5144b78ffc36a29bf79162b9
Mrigaank Sharma <mrigaanksharma928@gmail.com>
2025-06-19 12:46:44 +0530

    type script startd

---
+++ TypeScript/app.js
@@ -0,0 +1,23 @@
+//typescript demo start from here...
+//ye glt hai but valid hai js me 
+// var a=12;
+// a="mrigaank"
+//typescipt give type safety
+//valid js code is a valid typescript code...
+//if you want the code to compile simulatanoeusy as you code so use -- [tsc --watch]
+//demo day is going on 
+// var a:number=12;
+// var b:number=2402
+// function abcd(){
+//     return 2;
+// }
+// console.log(abcd())
+//basic types:...
+//.primitive [number,string,boolean]
+//12 23-number , p o l i c e-char , police-string , true false -boolean
+//primitive and references
+//{} [] () ye references hai changes parent me hoge changes....
+var a = [1, 2, 3];
+var b = a;
+b[0] = 5;
+console.log(b, a);
